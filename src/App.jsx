1a818ba8583c6dc365155656937b123ef68f532e
Mohamed Amr <M.Amr@ipic-eg.net>
2025-12-29 11:53:02 +0200

    User Authentication using Clerk and set scroll in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Routes from './Router/Routes'
+import React from "react";
+import Routes from "./Router/Routes";
+import { ClerkProvider } from "@clerk/clerk-react";
 
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 const App = () => {
   return (
-	<>
-	<Routes/>
-	</>
-  )
-}
+    <>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <Routes />
+      </ClerkProvider>
+    </>
+  );
+};
 
-export default App
+export default App;
