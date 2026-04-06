51c00e6a0855fa983b0cf02a2ead138dd0beccae
Mohamed Amr <M.Amr@ipic-eg.net>
2026-04-06 14:06:33 +0200

    make footer

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../component/Navbar'
 import { Outlet, useLocation } from 'react-router-dom'
+import Footer from '../component/Footer'
 
 const MainLayout = () => {
     const isOwnerPath = useLocation().pathname.includes("owner")
@@ -8,6 +9,7 @@ const MainLayout = () => {
     <>
     {!isOwnerPath && <Navbar/>}
     <Outlet/>
+    <Footer/>
     </>
   )
 }
